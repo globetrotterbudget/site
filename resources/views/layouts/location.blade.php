b28202a93d1543f0c9b5250b1e31157a384b5bfa
David R. Jex <david.jex@me.com>
2017-09-05 15:56:48 -0500

    Location view added and Page controller as well as a change to routes to mae the home page the location view.

---
+++ resources/views/layouts/location.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+	<title>Home</title>
+@stop
+
+@section('content')
+	<div class="container">
+    	<h1>Where would you like to go?</h1>
+   		<form method='GET' action='/days'>
+        <input class='form-control' type='text' name='location'>
+        <button  class='btn btn-success' type='submit'>Go</button>
+      </form>
+	</div>
+@stop
