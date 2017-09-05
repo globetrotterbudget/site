605c11c6948ee2171e2e36fc0526ed565771dc0b
David R. Jex <david.jex@me.com>
2017-09-05 12:33:33 -0500

    Created migration files for trips and entertainment.

---
+++ database/migrations/2017_09_05_171624_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('locations');
+            $table->integer('number_of_people');
+            $table->integer('number_of_days');
+            $table->integer('accomodations');
+            $table->integer('transportation');
+            $table->integer('food');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('trips');
+    }
+}
