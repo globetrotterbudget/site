3a483ccd0f223ce1257f3178925204a8a6a1e768
David <david.newton39@yahoo.com>
2017-09-06 14:10:37 -0500

    Created Entertainment Blade

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.master')
 
 @section('title')
+
 <title>Food and Dining</title>
 @stop
 
 @section('content')
 <h2>The "$$" Budget are for most meals at modestly priced restaurnts.</h2>
 <form method="POST" action="">
-		{{ csrf_field() }}
-		<button type="submit" name="$" value='lowest'>$</button>
+        {{ csrf_field() }}
+        <button type="submit" name="$" value='lowest'>$</button>
         <button type="submit" name="$$" value='modest'>$$</button>
         <button type="submit" name="$$$" value='highest'>$$$</button>
 </form>
