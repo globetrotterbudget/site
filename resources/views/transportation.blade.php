60d0838af8d0543226b8ea5dd6855c1f76adc24c
David <david.newton39@yahoo.com>
2017-09-06 10:16:35 -0500

    Created Transportation and Food Blade files

---
+++ resources/views/transportation.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title')
+<title>Transportation for Travel</title>
+@stop
+
+@section('content')
+<h2>How would you prefer to commute on your trip?</h2>
+<form method="POST" action="">
+            {{ csrf_field() }}
+            <button type="submit" name="public" value='public'>Public</button>
+            <button type="submit" name="rental" value='rental'>Rental</button>
+</form>
+@stop
