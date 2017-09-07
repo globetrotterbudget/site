dbe32c3a072e2593e309f0b52a01a208f529b507
Hunter Warren <hunter.warren@gmail.com>
2017-09-07 17:12:54 -0500

    Login/logout now works. Added a user controller. Added an account status page.

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -14,11 +14,24 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                	@if(!Auth::check())
+
                     <li><a href="">About</a></li>
-                    <li><a href="">Login</a></li>
+                    @if(!Auth::check())
+                    <li><a href="/auth/login">Login</a></li>
+                    @else
+                    <li><a href="#">My Trips</a></li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li><a href="{{ action('UsersController@show', array(Auth::id()) ) }}">Update Account</a></li>
+                        <li><a href="auth/logout">Logout</a></li>
+                        </ul>
+                    </li>
                     @endif
                 </ul>
+
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav> 
+                    
--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -13,7 +13,7 @@
 				<?php $location = array_shift($array); ?>
 			 	<h4 class="category">{{ $location }}</h4>
 			 	<a class="sidebarEdit" href="">edit</a>
-			 </div>
+			</div>
 			@foreach( $array as $key => $value )
 				<p>{{ $key . ':'}}</p>
 				<div class="row">
@@ -21,6 +21,15 @@
 				 	<a class="sidebarEdit" href="">edit</a>
 				 </div>
 			@endforeach
+		</div>
+	</div>
+	<div id="wizard" class="parent-container">
+		<div id="content">
+			
+        <form method="GET" action="{{action ('PageController@location')}}">
+          	<button type="submit">Add another trip</button>
+        </form>
+          	<button type="submit">Save this itinerary</button>
 		</div>
 	</div>
 </div>
