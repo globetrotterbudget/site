e082eb535db2d1477b97d88f308e48cb69b1c62d
Hunter Warren <hunter.warren@gmail.com>
2017-09-08 15:10:46 -0500

    added save functionality, updated migration file for trip.

---
+++ app/Trip.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    protected $table = 'trips';
+
+    protected $fillable = ['trip_name', 'locations', 'number_of_people', 'number_of_days', 'accomodations', 'transportation', 'food'];
+
+    
+}
