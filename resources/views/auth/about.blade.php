081947ec2e4c11e96f6b79dbd7ffd9dbedfc88e5
David <david.newton39@yahoo.com>
2017-09-18 15:46:40 -0500

    added profile pictures to About page

--- resources/views/auth/about.blade.php
+++ resources/views/auth/about.blade.php
@@ -7,19 +7,29 @@
 @section('content')
     
 <div class="container">
-
     <div id="login_box" class="col-md-offset-2 col-md-8 parent-container">
-        <div id="about">
+        <div id="aboutBlade"></div>
             <h2>ABOUT US</h2>
+                <div>
                 <p>We are Globetrotter, a full-stack development team that has created a high performance, feature-packed travel website for anyone interested in planning and budgeting their trip.
 
                 We believe everyone must have real time information which is essential in planning and budgeting your trip before you arrive at your destination. Even though you may have visited your destination in the past, you certainly may not have had the opportunity to see all the attractions and events that are to be seen. This is where Globetrotter will always be your best friend.
 
                 We have created designing features to give you real time currency exchange rates, hotel accommodations, local entertainment and transportation assuring you have everything you need for your trip at the touch of a button.</p>
                 <p>As always, feel free to let us know if you have any questions or concerns. Thank you.</p>
-                <p>Hunter Warren hunter.warren@gmail.com</p>
-                <p>David Jex david.jex@me.com</p>
-                <p>David Newton david.newton39@yahoo.com</p>
+                <div class="Row">
+                    <div class="C1"></div>
+                    <div class="C2"></div>
+                    <div class="C3"></div>
+                </div>
+                <div class="Row2">   
+                    <div class="colone">Hunter Warren hunter.warren@gmail.com</div>
+                    <div class="coltwo">David "Russell" Jex david.jex@me.com</div>
+                    <div class="colthree">David Newton david.newton39@yahoo.com</div>
+                </div>
+            
+            
+            </div>
         </div>
     </div>
 </div>
