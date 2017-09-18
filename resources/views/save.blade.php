219b64fbc5ea1d5b249ecc2e7ad34fd858e8fa56
Hunter Warren <hunter.warren@gmail.com>
2017-09-17 23:35:02 -0500

    summary, save, UI updates.

--- resources/views/save.blade.php
+++ resources/views/save.blade.php
@@ -11,28 +11,28 @@
 
  	<div class="row">
     	<div class="col-md-12">
-    		<h3>{{ $array['location'] }}</h3>
+    		<h3 class="tripLocation">{{ $array['location'] }}</h3>
     	</div>
     </div>
     <div class="row">
-    	<div class="col-md-3">
-    		<p>SELECTED:</p>
-	        <h5>Travelers: {{ $array['groupsize'] }}</h5>
-	        <h5 class="dayNumber" data-tripdays="{{ $array['days'] }}">Days: {{ $array['days'] }}</h5>
-	        <h5>Accommodations:</h5>
+    	<div class="col-md-4">
+    		<p class="costCategory">SELECTED:</p>
+	        <h5 class="costItem"><span class="indivCost">Travelers:</span> {{ $array['groupsize'] }}</h5>
+	        <h5 class="dayNumber costItem" data-tripdays="{{ $array['days'] }}"><span class="indivCost">Days:</span> {{ $array['days'] }}</h5>
+	        <h5 class="costItem"><span class="indivCost">Accommodations:</span></h5>
 	        <div class="starNumber" style="display:none">{{ $array['accommodations'] }}</div>
 	        <h5 class="starDisplay"></h5>
-	        <h5>Transportation:</h5>
-	        <h5>{{ $array['transportation'] }}</h5>
-	        <h5>Food / Drink:</h5>
+	        <h5 class="costItem"><span class="indivCost">Transportation:</span></h5>
+	        <h5 class="costItem">{{ $array['transportation'] }}</h5>
+	        <h5 class="costItem"><span class="indivCost">Food / Drink:</span></h5>
 	        <div class="dollarNumber" style="display:none">{{ $array['food'] }}</div>
 	        <h5 class="dollarDisplay"></h5>
-	        <h5>Entertainment extras:</h5>
+	        <h5 class="costItem"><span class="indivCost">Entertainment </extras:</h5>
 
     		@if(isset($entertainment) && is_array($entertainment))
 				@foreach($entertainment as $things)
 
-					<h5>{{ $things['description'] . ': '}}
+					<h5 class="costItem">{{ $things['description'] . ': '}}
 					{{ $things['price'] . ': '}}<br></h5>
 					<?php   $entTotal = 0;
 							$entTotal += (int)($things['price']); ?>
@@ -44,19 +44,19 @@
 			@endif
     	</div>
     	<div class="col-md-4">
-    		<p>COSTS:</p>
-    		<h5>Accommodations:</h5>
-    		<h5>${{ session()->get('average_accommodation_cost_per_day') }} per day</h5>
-    		<h5>${{ session()->get('average_accommodation_cost') }} per day</h5>
-    		<h5>Meals:</h5>
-    		<h5>${{ session()->get('average_food_cost_per_day') }} per day</h5>
-    		<h5>${{ session()->get('average_food_cost') }} group per day</h5>
-    		<h5>Transportation:</h5>
-    		<h5>${{ session()->get('average_transportation_cost_per_day') }} per day</h5>
-    		<h5>${{ session()->get('average_transportation_cost') }} group per day</h5>
+    		<p class="costCategory">COSTS:</p>
+    		<h5 class="costItem"><span class="indivCost">Accommodations:</span></h5>
+    		<h5 class="costItem">${{ session()->get('average_accommodation_cost_per_day') }} per day</h5>
+    		<h5 class="costItem">${{ session()->get('average_accommodation_cost') }} trip total</h5><br>
+    		<h5 class="costItem"><span class="indivCost">Meals:</span></h5>
+    		<h5 class="costItem">${{ session()->get('average_food_cost_per_day') }} per day</h5>
+    		<h5 class="costItem">${{ session()->get('average_food_cost') }} trip total</h5><br>
+    		<h5 class="costItem"><span class="indivCost">Transportation:</span></h5>
+    		<h5 class="costItem">${{ session()->get('average_transportation_cost_per_day') }} per day</h5>
+    		<h5 class="costItem">${{ session()->get('average_transportation_cost') }} trip total</h5>
 
     	</div>
-    	<div class="col-md-2">
+    	<div class="col-md-3 dailyColumn">
 <?php
     	$acc = (float)session()->get('average_accommodation_cost_per_day');
     	$food = (float)session()->get('average_food_cost_per_day');
@@ -68,37 +68,36 @@
     	$grouply = ($daily * $group);
     	?>
     		<h1 style="display:none" class="dailyNumber" data-daily="{{$daily}}">${{ $daily }}</h1>
-    		<h1>${{ number_format($daily,2,'.','') }}</h1>
-    		<p>per day</p>
-    		<h5>${{ number_format($grouply,2,'.','') }} group per day</h5>
+    		<h1 class="dailyNumber" style="display:inline-block">${{ number_format($daily,2,'.','') }} <p style="display:inline-block; font-size:16px"><span>/ day</span></p></h1>
+            
+            <h5 class="costItem"><span class="indivCost">${{ number_format($grouply,2,'.','') }}</span> group per day</h5>
+    	</div><br>
 
-    	</div>
-    	<div class="col-md-3">
-
-    		
-
-    	</div>
 
     <div class="col-md-12 parent-container">
-        <div id="save">
-        		<form method="POST" action="{{ action('PageController@store') }}">
+        <div id="save"><br>
+            <div class="col-md-4" style="padding:0px">
+        		<form style="display:inline-block" method="POST" action="{{ action('PageController@store') }}">
         		{{ csrf_field() }}
-        			<label for="save">Save to New Trip</label>
-        			<input type="text" name="trip_name">
-        			<button type="submit">Save</button>
+        			<label class="gtSave" for="save">Save to New Trip</label>
+        			<input class="gtInput" type="text" name="trip_name" placeholder="Enter a trip name" required>
+        			<button class="gtSaveButton" type="submit">Save</button>
         		</form>
-        		<form method="POST" action="{{ action('PageController@store') }}">
+            </div>
+            <div class="col-md-4" >
+        		<form style="display:inline-block" method="POST" action="{{ action('PageController@store') }}">
         		{{ csrf_field() }}
-					<label for="save">Save to Existing Trip</label>
-        			<select name="trip_name">
+					<label class="gtSave"for="save">Save to Existing Trip</label>
+        			<select class="gtInput" name="trip_name">
         		
 					<?php foreach($tripNames as $trip)
 					{
 						echo "<option>" . $trip->trip_name . "</option>";
 					}?>
 					</select>
-        			<button type="submit">Save</button>
-				
+        			<button class="gtSaveButton" type="submit">Save</button>
+			     </form>
+            </div>
 		</div>
 	</div>
 </div>
